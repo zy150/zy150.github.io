8cee935f31db7b90ba6aaa1e0bfb50445e8873ab
zy150 <zy150@users.noreply.github.com>
2025-05-08 13:20:30 +0000

    deploy: 2ddabfdfa0d78eff5a97334939552ccd57a0be2f

---
+++ assets/post.Buhw2QRn.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.BPTCviZO.js";import{n as t}from"./app.DnPoPYkr.js";import{M as e}from"./framework.XBYO8o58.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const n=r.value.types[o].color,s=r.value.types[o].icon,i=e(()=>o?{"--card-c-primary":n}:void 0);return{color:n,icon:s,styles:i}}export{m as u};
